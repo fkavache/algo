04cafc91af0401a28411ed504792484f5b129b54
fkavache <vkats15@freeuni.edu.ge>
2021-05-13 14:45:38 +0400

    added LCP967

--- LeetCode/Recursion/967. Numbers With Diff.cpp
+++ LeetCode/Recursion/967. Numbers With Diff.cpp
@@ -1,41 +1,20 @@
-void insert(unordered_set<string> &set, string val, int n) {
-    if (val[0] != '0' && val.length() == n)
-        set.insert(val);
-}
-
-unordered_set<string> numsSameInternal(int n, int k) {
-    unordered_set<string> res;
-    if (n == 1) {
-        for (int i = 0; i < 10 - k; i++) res.insert(to_string(i));
-        return res;
-    }
-
-    unordered_set<string> rec = numsSameInternal(n - 1, k);
-    for (string i : rec) {
-        int l = i[n-2] - '0';
-        int f = i[0]   - '0';
-        if (l + k < 10) insert(res, i + to_string(l + k), n);
-        if (l - k >= 0) insert(res, i + to_string(l - k), n);
-        if (f + k < 10) insert(res, to_string(f + k) + i, n);
-        if (f - k >= 0) insert(res, to_string(f - k) + i, n);
-    }
-
-    return res;
-}
-
 vector<int> numsSameConsecDiff(int n, int k) {
     vector<int> res;
+    if (n == 1) return {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    unordered_set<string> rec_res = numsSameInternal(n, k);
-    for (auto const &i: rec_res) res.push_back(stoi(i));
-
+    vector<int> rec = numsSameConsecDiff(n - 1, k);
+    for (int i : rec) {
+        int l = i % 10;
+        if (l + k >= 0 && l + k < 10)           res.push_back(i * 10 + l + k);
+        if (l - k >= 0 && l - k < 10 && k != 0) res.push_back(i * 10 + l - k);
+    }
     return res;
 }
 
 int main() {
-    vector<int> res = numsSameConsecDiff(2, 2);
-
-    for (int re: res) cout << re << ", ";
-
+    vector<int> res = numsSameConsecDiff(2, 1);
+    for (int i: res) {
+        cout << i << ", ";
+    }
     return 0;
 }
